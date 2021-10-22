9df7868304903da200fdf20901ea62b6fcd34914
Ethanil <ethanil@pop-os.localdomain>
2021-10-22 02:23:50 +0200

    vault backup: 2021-10-22 02:23:50

--- Informatik/myScripts/test.py
+++ Informatik/myScripts/test.py
@@ -1,5 +1,11 @@
 from mdutils.mdutils import MdUtils
-import gcalcli
+import os
+import datetime
 #mdFile = MdUtils(file_name='Example_Markdown',title='Markdown File Example')
 #mdFile.create_md_file()
-gcalcli.cli.main()
+stream=os.popen('gcalcli --calendar uni --nocolor  agenda')
+output=stream.read()
+today = datetime.datetime.now().strftime("%a %b %d")
+nextDay = output[1:].split('\n\n')[0]
+if today in nextDay:
+    print(nextDay[len(today):])
