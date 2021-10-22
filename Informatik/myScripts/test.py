3a6331f988a8deafc4d45e7d0a8e29f0432ede72
Ethanil <ethanil@pop-os.localdomain>
2021-10-22 08:04:54 +0200

    vault backup: 2021-10-22 08:04:54

--- Informatik/myScripts/test.py
+++ Informatik/myScripts/test.py
@@ -6,14 +6,13 @@ output=stream.read()
 todaysDate=datetime.datetime.now()
 today = todaysDate.strftime("%a %b %d")
 nextDay = output[1:].split('\n\n')[0]
+mdFile = MdUtils(file_name='Day Planner-'+todaysDate.strftime("%Y%m%d"),title=todaysDate.strftime("%d.%m.%Y"))
+mdFile.new_line("| Gestern | Morgen |")
+mdFile.new_line("| ------- | ------ |")
+mdFile.new_line("| [[Day Planner-{}]] | [[Day Planner-{}]] |".format((todaysDate+datetime.timedelta(days=-1)).strftime("%Y%m%d"),(todaysDate+datetime.timedelta(days=1)).strftime("%Y%m%d")))
 if today in nextDay:
     markdown = nextDay[len(today):].split('\n')
     for line in markdown:
-        print("[ ] "+line.strip().replace("          "," "))
-
-table="""| Gestern                                           | Morgen                                           |
-| ------------------------------------------------- | ------------------------------------------------ |
-| [[Day Planner-{}]] | [[Day Planner-{}]] |
-""".format(todaysDate+datetime.timedelta(days=-1).strftime("%Y%m%d"),todaysDate+datetime.timedelta(days=1).strftime("%Y%m%d"))
-mdFile = MdUtils(file_name='Day Planner-'+todaysDate.strftime("%Y%m%d"),title=todaysDate.strftime("%d.%m.%Y"))
-#mdFile.create_md_file()
+        line = "[ ] "+line.strip().replace("          "," ")
+        mdFile.new_line(line)
+mdFile.create_md_file()
